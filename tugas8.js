d0bb796e65db6fdd324ccafe3f55d7b957dad39d
Havid <great_havid@yahoo.co.id>
2019-11-11 14:22:40 +0700

    Tugas 8 JS

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+// ====== Tugas 8 JavaScript =====
+
+console.log("Nilai Bootcamp")
+
+function init() {
+	var Bootcamp = {
+		inisialA: 60,
+		inisialB: "Belum Mengumpulkan",
+		inisialC: 85,
+	}
+	console.log(Bootcamp)
+	console.log("Revisi Bootcamp", Bootcamp)
+}
+
+init()
